7f94fc6c8752b94b96ad0c39eeed6ac0d212877e
Anisur Rahman Mahmud <19202103107@cse.bubt.edu.bd>
2023-03-10 15:48:32 +0600

    added border control and couple of game objects

--- spacewar.py
+++ spacewar.py
@@ -19,6 +19,19 @@ class Sprite(turtle.Turtle):
         self.speed = 1
     def move(self):
         self.forward(self.speed)
+        #boundary
+        if self.xcor() >290:
+            self.setx(290)
+            self.right(60)
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.right(60)
+        if self.ycor() >290:
+            self.sety(290)
+            self.right(60)
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.right(60)
 class Player(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
         Sprite.__init__(self, spriteshape, color, startx, starty)
@@ -32,6 +45,31 @@ class Player(Sprite):
         self.speed +=1
     def breaks(self):
         self.speed -=1
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+    def draw_border(self):
+        #draw border
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300,300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.forward(600)
+            self.pen.right(90)
+        self.pen.penup()
+        self.pen.hideturtle()
+#game object
+game = Game()
+#game border
+game.draw_border()
+
 #sprite creations
 player = Player("triangle","white",0,0)
 
