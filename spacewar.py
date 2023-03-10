fdb0b20e5ec4526cb9b78c2d6f335055cdc5f432
Anisur Rahman Mahmud <19202103107@cse.bubt.edu.bd>
2023-03-10 15:19:49 +0600

    keyboard bindings
    added support for the player to move as per key pressed. Old school games shit.

--- spacewar.py
+++ spacewar.py
@@ -2,11 +2,11 @@ import turtle
 import os
 import random
 #turtle.fd(0)
-turtle.speed(0)
-turtle.bgcolor("black")
+turtle.speed(0)#animation speed
+turtle.bgcolor("black")#background color
 turtle.hideturtle()
-turtle.setundobuffer(1)
-turtle.tracer(1)
+turtle.setundobuffer(1)#saves memory
+turtle.tracer(1)#speeds up drawing
 
 class Sprite(turtle.Turtle):
     def __init__(self, spriteshape, color, startx, starty):
@@ -17,7 +17,33 @@ class Sprite(turtle.Turtle):
         #self.fd(0)
         self.goto(startx,starty)
         self.speed = 1
+    def move(self):
+        self.forward(self.speed)
+class Player(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 4
+        self.lives = 3
+    def turn_left(self):
+        self.left(45)
+    def turn_right(self):
+        self.right(45)
+    def accelerate(self):
+        self.speed +=1
+    def breaks(self):
+        self.speed -=1
 #sprite creations
-player = Sprite("triangle","white",0,0)
+player = Player("triangle","white",0,0)
+
+#keyboard settings
+turtle.onkey(player.turn_left,"Left")
+turtle.onkey(player.turn_right,"Right")
+turtle.onkey(player.accelerate,"Up")
+turtle.onkey(player.breaks,"Down")
+turtle.listen()
+
+#game
+while True:
+    player.move()
 
 delay = input("Press enter to finish. >")
